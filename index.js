bc4784047eaae0467f2304908b8eb69fd53990b6
emilywhitaker15@gmail.com <emilywhitaker15@gmail.com>
2018-05-21 15:23:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(arrayMusicians, arrayInstruments) {
   var lennonPlays = []
   for (i = 0; i < arrayMusicians.length; i++) {
-    lennonPlays.
+    lennonPlays.push(`${arrayMusicians[i]} plays ${arrayInstruments[i]}`)
   }
+  return 
 }
